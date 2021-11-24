2053dd1b91f29c6851a2c5bae5155ffd7b04d35f
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-11-24 06:37:59 +0500

    💻 CODING: Wrap App with store and invoke setupServer function if the the environment is development

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { setupServer } from './services/mirage/server';
+import store from './store';
+import { Provider } from 'react-redux';
+
+if (process.env.NODE_ENV === 'development') {
+  setupServer();
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
