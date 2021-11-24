00234145c57c893f2beac6c3fbaef53c237d5b92
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-11-24 06:47:13 +0500

    💻 CODING: Create home page

---
+++ src/features/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Diaries from '../diary/Diaries';
+import Editor from '../entry/editor';
+
+const Home = () => {
+    return (
+        <div>
+            <Diaries />
+        </div>
+    )
+}
+
+export default Home;
